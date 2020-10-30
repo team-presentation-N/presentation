b633dad59e959166181bb87e90389b21075b26d9
t-ikeda <t-ikeda@newral.co.jp>
2020-10-30 12:48:06 +0900

    06発表後

--- ikeda/C#の発表/説明用code/説明_015/説明_015/Program.cs
+++ ikeda/C#の発表/説明用code/説明_015/説明_015/Program.cs
@@ -7,14 +7,20 @@ namespace 説明_015
     {
         static void Main()
         {
-            var intlist = new List<int>();
-
             var floatdict = new Dictionary<string, float>();
 
             floatdict.Add("円周率", 3.141F);
             floatdict.Add("自然対数の底", 2.718F);
-            Console.WriteLine(floatdict["円周率"]);
 
+            /*
+            var floatdict = new Dictionary<string, float>
+            {
+                { "円周率", 3.141F },
+                { "自然対数の底", 2.718F }
+            };
+            */
+
+            Console.WriteLine(floatdict["円周率"]);
         }
     }
 }
---
+++ ikeda/C#の発表/説明用code/説明_016/説明_016/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace 説明_016
+{
+    //int型を受け,戻り値のないメソッドを代表するdelegateの型"Testdele"を定義
+    delegate void Testdele(int a);
+
+    class MainClass
+    {
+        static void Main()
+        {
+            var inst = new SubClass();
+
+            //Testdele型の変数"td"を宣言し,SCmethod1を登録
+            var td = new Testdele(inst.SCmethod1);
+            td(10);
+            Console.WriteLine();
+
+            //tdにSCmethod2を登録(元のSCmethod1は上書きされる)
+            td = inst.SCmethod2;
+            td(10);
+            Console.WriteLine();
+
+            //tdを変数として扱う
+            Delexe(td);
+            Console.WriteLine();
+        }
+
+        static void Delexe(Testdele td)
+        {
+            td(16);
+            td(22);
+        }
+    }
+
+    class SubClass
+    {
+        public void SCmethod1(int a)
+        {
+            Console.WriteLine(2 * a);
+        }
+        public void SCmethod2(int a)
+        {
+            Console.WriteLine(a / 2);
+        }
+    }
+}
