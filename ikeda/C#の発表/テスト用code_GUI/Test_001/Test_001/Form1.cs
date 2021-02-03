b2b619f724b6bad271f95e257cce940c38348ef5
t-ikeda <t-ikeda@newral.co.jp>
2021-02-03 13:28:14 +0900

    GUIアプリに関する項目を追加

---
+++ ikeda/C#の発表/テスト用code_GUI/Test_001/Test_001/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new Exception("メインスレッド例外");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => throw new Exception("別スレッド例外"));
+        }
+    }
+}
---
+++ ikeda/C#の発表/テスト用code_GUI/Test_001/Test_001/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_001
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            /*
+            //メインスレッドで発生し,ハンドルされなかった例外を処理するThreadExceptionイベントハンドラを追加
+            Application.ThreadException +=
+                new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            */
+
+            //App.configの設定に依らず,ThreadExceptionが発生しないようにする
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
+
+            //他スレッドで発生し,ハンドルされなかった例外を処理するUnhandledExceptionイベントハンドラを追加
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Program_UnhandledException);
+
+            Application.Run(new Form1());
+        }
+
+        /*
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            try
+            {
+                //エラーメッセージをウィンドウに表示する
+                MessageBox.Show(e.Exception.Message, "ThreadException");
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+        */
+
+        static void Program_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                //エラーメッセージをウィンドウに表示する
+                MessageBox.Show(((Exception)e.ExceptionObject).Message, "UnhandledException");
+            }
+            finally
+            {
+                //アプリケーションを終了する
+                Environment.Exit(1);
+            }
+        }
+    }
+}
