c1f0986753e443dbac38b931f55a049c99991564
t-ikeda-home <t-ikeda@newral.co.jp>
2021-02-15 16:45:24 +0900

    プロセス間通信(IPC)関連を追加

---
+++ ikeda/C#の発表/テスト用code/IPCtest/IPC_Client/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Ipc;
+using ShareSpace;
+
+namespace IPC_Test
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            IpcClient ipcc = new IpcClient();
+            if (ipcc.Shareobj.Str == null)
+            {
+                ipcc.Shareobj.Str = "未設定";
+            }
+
+            while (true)
+            {
+                /*
+                Console.WriteLine("キー入力で共有オブジェクトのメンバーを表示");
+                Console.ReadKey();
+                */
+                Console.WriteLine("受信時刻：" + DateTime.Now.ToString() + " " + ipcc.Shareobj.Str);
+            }
+        }
+    }
+
+    class IpcClient
+    {
+        public IpcRemoteObject Shareobj { get; private set; }
+
+        public IpcClient()
+        {
+            //クライアントチャンネルの生成
+            IpcClientChannel channel = new IpcClientChannel();
+
+            //チャンネルを登録
+            ChannelServices.RegisterChannel(channel, true);
+
+            //"ipc://channel_name/testurl"からオブジェクトを取得
+            Shareobj = Activator.GetObject(typeof(IpcRemoteObject), "ipc://channel_name/testurl") as IpcRemoteObject;
+        }
+    }
+}
+
+namespace ShareSpace
+{
+    //共有されるオブジェクト
+    public class IpcRemoteObject : MarshalByRefObject
+    {
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+        public string Str { get; set; }
+    }
+}
---
+++ ikeda/C#の発表/テスト用code/IPCtest/IPC_Server/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Ipc;
+using ShareSpace;
+
+namespace IPCtest
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            IpcServer ipcs = new IpcServer();
+            if (ipcs.Shareobj.Str == null)
+            {
+                ipcs.Shareobj.Str = "未設定";
+            }
+
+            while (true)
+            {
+                Console.WriteLine("キー入力で共有オブジェクトのメンバーを更新");
+                ipcs.Shareobj.Str = Console.ReadLine();
+                Console.WriteLine("設定時刻：" + DateTime.Now.ToString());
+            }
+        }
+    }
+
+    //IPCサーバー
+    class IpcServer
+    {
+        public IpcRemoteObject Shareobj { get; private set; }
+
+        public IpcServer()
+        {
+            //サーバーチャンネルの生成
+            IpcServerChannel channel = new IpcServerChannel("channel_name");
+
+            //チャンネルを登録
+            ChannelServices.RegisterChannel(channel, true);
+
+            //"ipc://channel_name/testurl"にオブジェクトを公開
+            Shareobj = new IpcRemoteObject();
+            RemotingServices.Marshal(Shareobj, "testurl", typeof(IpcRemoteObject));
+        }
+    }
+}
+
+namespace ShareSpace
+{
+    //共有されるオブジェクト
+    public class IpcRemoteObject : MarshalByRefObject
+    {
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+        public string Str { get; set; }
+    }
+}
+
